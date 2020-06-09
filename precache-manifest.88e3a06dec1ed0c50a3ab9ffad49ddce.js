04d49bf5bc97b8d1b8308a2c01eb1863595a2204
Chris Troupe <christophertroupe@gmail.com>
2020-06-09 16:22:11 -0500

    Updates

---
+++ precache-manifest.88e3a06dec1ed0c50a3ab9ffad49ddce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f410b034b5c5e741fa5a2384e6d9d5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "aa0b09ac9e5f362823dc",
+    "url": "/my-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "04f375ad080947141ada",
+    "url": "/my-app/static/css/main.8fb1d611.chunk.css"
+  },
+  {
+    "revision": "aa0b09ac9e5f362823dc",
+    "url": "/my-app/static/js/2.c74fc0ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.c74fc0ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f375ad080947141ada",
+    "url": "/my-app/static/js/main.682ae3f8.chunk.js"
+  },
+  {
+    "revision": "52724b1565d9b170352e",
+    "url": "/my-app/static/js/runtime-main.fc3561af.js"
+  }
+]);
